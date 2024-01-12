d461afe22cfeb43f7e0fdd0f507ac4b85a2bc567
千年八云紫 <herobrinereimu@gmail.com>
2024-01-12 15:37:54 +0800

    frequency_penalty调整为0.2

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def translate_text(text, index, repetitive_part=None, attempt=1):
     # 构造POST请求的数据
     if ver == 1 :
         data = {
-            "frequency_penalty": 0,
+            "frequency_penalty": 0.2,
             "n_predict": 1000,
             "prompt": f"<|im_start|>system\n你是一个轻小说翻译模型，可以流畅通顺地以日本轻小说的风格将日文翻译成简体中文，并联系上下文正确使用人称代词，不擅自添加原文中没有的代词。<|im_end|>\n<|im_start|>user\n将下面的日文文本翻译成中文：{modified_text}<|im_end|>\n<|im_start|>assistant\n",
             "repeat_penalty": 1,
@@ -72,7 +72,7 @@ def translate_text(text, index, repetitive_part=None, attempt=1):
         }
     else:
         data = {
-            "frequency_penalty": 0,
+            "frequency_penalty": 0.2,
             "n_predict": 1000,
             "prompt": f"<reserved_106>将下面的日文文本翻译成中文：{modified_text}<reserved_107>",
             "repeat_penalty": 1,
